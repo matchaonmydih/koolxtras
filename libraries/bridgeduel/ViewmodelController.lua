e5626e6a52aa6870db9ca78a21d5e9acfbefbdcb
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-07 22:48:22 +1000

    Update ViewmodelController.lua

--- libraries/bridgeduel/ViewmodelController.lua
+++ libraries/bridgeduel/ViewmodelController.lua
@@ -12,34 +12,35 @@ return {
         return workspace.CurrentCamera
     end,
     PlayAnimation = function(self, tool)
-        if not self:GetContainer().Viewmodel:FindFirstChild('Viewmodel') then return end
-            
-        local toolnme, animObj = nil, self:GetContainer().Viewmodel:WaitForChild(tool, 10)
-        if not animObj then return end
-        
-        local anim = animObj.Animation
-        
-        if tool:find('Sword') then
-            toolnme = 'Sword'
-        elseif tool:find('Pickaxe') then
-            toolnme = 'Pickaxe'
-        elseif tool:find('Potion') then
-            toolnme = 'Potion'
-        else
-            toolnme = tool
-        end
-        anim.AnimationId = animations[toolnme]
-    
-        local track = animObj.AnimationController.Animator:LoadAnimation(anim)
-        track.Name = 'ToolAnimation'
-    
-        for _, v in animObj.AnimationController.Animator:GetPlayingAnimationTracks() do
+        local toolnme
+		if not self:GetContainer():FindFirstChild('Viewmodel') then return end
+
+		local animObj = self:GetContainer().Viewmodel:WaitForChild(tool, 10)
+		if not animObj then return end
+
+		local anim = animObj.Animation
+
+		if tool:find('Sword') then
+			toolnme = 'Sword'
+		elseif tool:find('Pickaxe') then
+			toolnme = 'Pickaxe'
+		elseif tool:find('Potion') then
+			toolnme = 'Potion'
+		else
+			toolnme = tool
+		end
+		anim.AnimationId = animations[toolnme]
+
+		local track = animObj.AnimationController.Animator:LoadAnimation(anim)
+		track.Name = 'ToolAnimation'
+
+		for _, v in animObj.AnimationController.Animator:GetPlayingAnimationTracks() do
 			if v.Name == 'ToolAnimation' and (tool:find('Sword') or tool:find('Pickaxe')) then
 				v.TimePosition = 0
 				v:Destroy()
 			end
 		end
-		
+
 		track:Play()
 		return track
     end
