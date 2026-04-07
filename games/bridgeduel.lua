4941f4c241b2381b043b3cd4854f2d663d4c7cdc
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-07 16:34:06 +1000

    Update bridgeduel.lua

--- games/bridgeduel.lua
+++ games/bridgeduel.lua
@@ -32,7 +32,7 @@ local Entity = loadstring(downloadFile('koolaid/libraries/entity.lua'))()
 
 local Dependencies = {
     Blink = Functions.require(ReplicatedStorage.Blink.Client),
-	Detections = module.requirejank.helper:Fetch('Detections'),
+	Detections = Functions.requirejank.helper:Fetch('Detections'),
 	Entity = Functions.require(ReplicatedStorage.Modules.Entity),
 	Paths = {
 		Knockback = ReplicatedStorage.Modules.Knit.Services.CombatService.RE.KnockBackApplied
