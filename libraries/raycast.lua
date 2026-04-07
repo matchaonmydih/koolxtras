03925cacab3c6b6fc44c56035746e2a15be6f874
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-07 22:40:58 +1000

    Update raycast.lua

--- libraries/raycast.lua
+++ libraries/raycast.lua
@@ -16,9 +16,9 @@ function raycast:CanSee(target, filter)
     local rayParams, res = RaycastParams.new(), nil
     rayParams.FilterType = Enum.RaycastFilterType.Exclude
     rayParams.FilterDescendantsInstances = filter or {lplr.Character}
-
-    res = workspace:Raycast(lplr.Character.HumanoidRootPart.Position, target.Position - lplr.Character.HumanoidRootPart.Position, rayParams)
-    return res and res.Instance == target
+   	local res = workspace:Raycast(lplr.Character.HumanoidRootPart.Position, target.Position - lplr.Character.HumanoidRootPart.Position, rayParams)
+	
+    return (res and res.Instance and not target:IsAncestorOf(res.Instance)) and false or true
 end
 
 return raycast
