b05f263aa35b98d6ead34e4362513ae19f55a40a
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-07 19:09:56 +1000

    Update functions.lua

--- libraries/functions.lua
+++ libraries/functions.lua
@@ -1,8 +1,9 @@
 --[[
-    Credits to the @renderintents team over on github for making the hook functionality work in luau
 
-    https://github.com/renderintents
-    https://github.com/renderintents/Render/blob/main/libraries/utils.luau
+    Credits to @Experience_Member and @SQLanguage on the DevForum for making hookfunction possible!
+    Experience_Member: https://devforum.roblox.com/u/Experience_Member
+    SQLanguage: https://devforum.roblox.com/u/SQLanguage
+
 ]]
 
 local module = {
@@ -26,22 +27,208 @@ local Services = setmetatable({}, {
 local Players = Services.Players
 local lplr = Players.LocalPlayer
 
-module.isfile = isfile or function(file: string): string
-	local suc, ret = pcall(function()
-		return readfile(file)
+type userdata = {}
+type _function = (...any) -> (...any)
+
+local Metatable = {
+	metamethods = {
+		__index = function(self, key)
+			return self[key]
+		end,
+		__newindex = function(self, key, value)
+			self[key] = value
+		end,
+		__call = function(self, ...)
+			return self(...)
+		end,
+		__concat = function(self, b)
+			return self..b
+		end,
+		__add = function(self, b)
+			return self + b
+		end,
+		__sub = function(self, b)
+			return self - b
+		end,
+		__mul = function(self, b)
+			return self * b
+		end,
+		__div = function(self, b)
+			return self / b
+		end,
+		__idiv = function(self, b)
+			return self // b
+		end,
+		__mod = function(self, b)
+			return self % b
+		end,
+		__pow = function(self, b)
+			return self ^ b
+		end,
+		__tostring = function(self)
+			return tostring(self)
+		end,
+		__eq = function(self, b)
+			return self == b
+		end,
+		__lt = function(self, b)
+			return self < b
+		end,
+		__le = function(self, b)
+			return self <= b
+		end,
+		__len = function(self)
+			return #self
+		end,
+		__iter = function(self)
+			return next, self
+		end,
+		__namecall = function(self, ...)
+			return self:_(...)
+		end,
+		__metatable = function(self)
+			return getmetatable(self)
+		end
+	}
+}
+
+-- methods
+function Metatable.get_L_closure(metamethod: string, obj: {any} | userdata)
+	local hooked
+	local metamethod_emulator = Metatable.metamethods[metamethod]
+	
+	xpcall(function()
+		metamethod_emulator(obj)
+	end, function()
+		hooked = debug.info(2, "f")
 	end)
-
-	return suc and true or false
+	
+	return hooked
 end
 
-module.hook = hookfunction or hookfunc or hook_function or function(old, new)
-    pcall(function()
-        getfenv()[debug.info(old, 'n')] = new
-    end)
+function Metatable.get_all_L_closures(obj: {any} | userdata)
+	local metamethods = {}
+	local innacurate = {}
+
+	for method, _ in Metatable.metamethods do
+		local metamethod, accurate = Metatable.get_L_closure(method, obj)
+		metamethods[method] = metamethod
+	end
+
+	return metamethods
 end
 
-module.restorefunc = restorefunction or restorefunc or restore_function or function(func)
-    module.hook(func, func)
+function Metatable.metahook(t: any, f: _function)
+	local metahook = {
+		__Metatable = getmetatable(t) or "The metatable is locked"
+	}
+
+	for metamethod, value in Metatable.metamethods do
+		metahook[metamethod] = function(self, ...)
+			f(metamethod, ...)
+			
+			if metamethod == "__tostring" then
+				return ""
+			elseif metamethod == "__len" then
+				return math.random(0, 1024)
+			end
+			
+			return Metatable.metahook({}, f) 
+		end
+	end
+
+	return setmetatable({}, metahook)
+end
+
+local scheduled_tasks = {}
+
+task.spawn(function()
+	while task.wait() do
+		for i, new_task in scheduled_tasks do
+			scheduled_tasks[i] = nil
+			
+			task.spawn(new_task)
+		end
+	end
+end)
+
+--// Main
+module.hook = hookfunction or hook_function or hookfunc or function(old, new, run_on_seperate_thread)
+	if debug.info(old, "s") == "[C]" then
+		local name = debug.info(old, "n")
+		
+		return function(...)
+			return old(...)
+		end
+	else
+
+		local last_trace
+		
+		local function execute_hook()
+			if new then
+				local current_trace = {
+					debug.info(3, "l"), debug.info(3, "s"), debug.info(3, "n"), debug.traceback()
+				}
+				
+				local equal = true
+				for i, v in last_trace or {} do
+					if current_trace[i] ~= v then
+						equal = false
+					end
+				end
+				
+				if not equal or not last_trace then
+					if run_on_seperate_thread then
+						table.insert(scheduled_tasks, coroutine.wrap(new))
+					else
+						new()
+					end
+				end
+				
+				return current_trace
+			end
+		end
+		
+		local function wrap()
+			local hooks = {}
+			
+			for metamethod in Metatable.metamethods do
+				hooks[metamethod] = function(self, ...)
+					local f = debug.info(2, "f")
+
+					if f == old then
+						last_trace = execute_hook()
+					end
+					
+					if metamethod == "__len" then
+						return 3
+					elseif metamethod == "__tostring" then
+						return tostring(getfenv(0))
+					end
+					
+					return wrap()
+				end
+			end
+			
+			return setmetatable({}, hooks)
+		end
+		
+		local environment = wrap()
+		setfenv(old, environment)
+		
+		return function(...)
+			local return_value = {old(...)}
+			
+			while not return_value do task.wait() end
+			setfenv(old, wrap()) -- insert new hook once old gets executed
+			
+			return unpack(return_value)
+		end
+	end
+end
+
+module.hookmetamethod = hookmetamethod or function(obj, metamethod, func)
+	module.hook(old[metamethod], func)
 end
 
 -- Require
