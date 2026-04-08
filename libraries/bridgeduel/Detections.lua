6e400d8bf7ad32d7a459560689258e4deb2f96a9
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-08 11:03:58 +1000

    Update Detections.lua

--- libraries/bridgeduel/Detections.lua
+++ libraries/bridgeduel/Detections.lua
@@ -10,6 +10,18 @@ local Services = setmetatable({}, {
 
 local ReplicatedStorage = Services.ReplicatedStorage
 
+local function downloadFile(file)
+    url = file:gsub('koolaid/', '')
+    if not isfile(file) then
+        writefile(file, game:HttpGet('https://raw.githubusercontent.com/sstvskids/koolxtras/'..readfile('koolaid/commit.txt')..'/'..url))
+    end
+
+    repeat task.wait() until isfile(file)
+    return readfile(file)
+end
+
+local Crypt = loadstring(downloadFile('koolaid/libraries/Crypt.lua'))()
+
 local Detections = {
     Paths = {
         SendReport = ReplicatedStorage.Modules.Knit.Services.NetworkService.RF.SendReport,
@@ -22,8 +34,19 @@ local Detections = {
     Count = 0
 }
 
+local function getscripthash(path: Instance)
+	if not path then return nil end
+	if not getscriptbytecode then return nil end
+
+	local bytecode = getscriptbytecode(path)
+	if not bytecode then return nil end
+
+	return Crypt.sha384(bytecode)
+end
+
 function Detections:test(typee)
 	if typee == 'hash' and getscripthash then
+		setclipboard(getscripthash(Detections.Paths.Client.Sword))
 		if getscripthash(Detections.Paths.Client.Sword) ~= '16D1A471E2AF4F32DA974993CF13D7ACA4EB8BB15B3C7C31E494F02DB0D323A3' then
 	        Detections.Count += 1
 			Detections.Logs.SwordH = 'SwordClient hash was not the same (Dev\'s changed the hash -- report this to kool aid devs)'
