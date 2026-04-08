ef65f4b02048de195c9fbd3e28dc670f89092a2f
sstvskids <staavstudios@gmail.com>
2026-04-08 20:43:07 +1000

    Add BedFight Support

---
+++ games/bedfight.lua
@@ -0,0 +1,129 @@
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local Services = setmetatable({}, {
+	__index = function(self, obj)
+		return cloneref(game:GetService(obj))
+	end
+})
+
+local ReplicatedStorage = Services.ReplicatedStorage
+local UserInputService = Services.UserInputService
+local HttpService = Services.HttpService
+local Players = Services.Players
+local lplr = Players.LocalPlayer
+
+local function downloadFile(file)
+    url = file:gsub('koolaid/', '')
+    if not isfile(file) then
+        writefile(file, game:HttpGet('https://raw.githubusercontent.com/sstvskids/koolxtras/'..readfile('koolaid/commit.txt')..'/'..url))
+    end
+
+    repeat task.wait() until isfile(file)
+    return readfile(file)
+end
+
+local Library = shared.Library
+
+local Functions = loadstring(downloadFile('koolaid/libraries/functions.lua'))()
+local Raycast = loadstring(downloadFile('koolaid/libraries/raycast.lua'))()
+local Entity = loadstring(downloadFile('koolaid/libraries/entity.lua'))()
+
+local Dependencies = {
+    Modules = {
+        ItemData = Functions.require(ReplicatedStorage.Modules.DataModules.ItemsData),
+        VeloUtils = ReplicatedStorage.Modules.VelocityUtils
+    },
+    Classes = {
+        Melee = {},
+        Ranged = {},
+        Mining = {}
+        Blocks = {}
+    }
+}
+
+--[[
+
+    For future stav: Aura is going to be set out by the names of items in the table of classes
+    Melee = {
+        Wooden Sword, etc.
+    }
+
+    Get the items in those classes, see if the player has the item on them, attack using the names of the classes
+
+]]
+
+function entity:GetClosestPlayer(range, angle, wallcheck)
+    local minrnge, entity = range, nil
+
+	for i,v in Players:GetPlayers() do
+	    if v ~= lplr and self.isAlive(lplr) and self.isAlive(v) then
+			if lplr:GetAttribute('PVP') == false or v:GetAttribute('PVP') == false then continue end
+	        if wallcheck and not raycast:CanSee(v.Character.PrimaryPart, {lplr.Character}) then continue end
+	        if v.Team and lplr.Team and v.Team == lplr.Team then continue end
+
+		    local plrdir = math.deg(lplr.Character.HumanoidRootPart.CFrame.LookVector:Angle((v.Character.PrimaryPart.Position - lplr.Character.PrimaryPart.Position).Unit))
+		    if angle <= plrdir / 2 then continue end
+
+			local dist = lplr:DistanceFromCharacter(v.Character.PrimaryPart.Position)
+			if dist < minrnge then
+			    minrnge = dist
+			    entity = v
+			end
+	    end
+	end
+
+    return entity
+end
+
+local classGroups = {
+    Swords = 'Melee', Mace = 'Melee', Katanas = 'Melee', Daggers = 'Melee', ['Battle Axes'] = 'Melee', Spears = 'Melee',
+    Axes = 'Mining', Pickaxes = 'Mining',
+    Ranged = 'Ranged',
+    Blocks = 'Blocks'
+}
+
+for i,v in Dependencies.Modules.ItemData do
+    if classGroups[v.Class] then
+        table.insert(Dependencies.Classes[classGroups[v.Class]], i)
+        continue
+    end
+end
+
+do
+    local AntiKB, AntiKBConn
+    AntiKB = Library.Tabs.Combat:CreateModule({
+        Name = 'AntiKB',
+        Function = function(callback)
+            if callback then
+                for i,v in Dependencies.Modules.VeloUtils:GetChildren() do
+                    if v.ClassName == 'LinearVelocity' then
+                        v.Enabled = false
+                    end
+
+                    continue
+                end
+
+                AntiKBConn = Dependencies.Modules.VeloUtils.ChildAdded:Connect(function(obj)
+                    if obj.ClassName == 'LinearVelocity' then
+                        obj.Enabled = false
+                    end
+                end)
+            else
+                if AntiKBConn then
+                    AntiKBConn:Disconnect()
+                    AntiKBConn = nil
+                end
+
+                for i,v in Dependencies.Modules.VeloUtils:GetChildren() do
+                    if v.ClassName == 'LinearVelocity' then
+                        v.Enabled = true
+                    end
+
+                    continue
+                end
+            end
+        end
+    })
+end
---
+++ libraries/bedfight/ItemsData.lua
@@ -0,0 +1,531 @@
+-- Decompiled with Velocity Script Decompiler
+local v1 = {
+    ["Wooden Sword"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 75265976270703,
+        ["Class"] = "Swords"
+    },
+    ["Stone Sword"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 112353849640146,
+        ["Class"] = "Swords"
+    },
+    ["Iron Sword"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 82149767422606,
+        ["Class"] = "Swords"
+    },
+    ["Diamond Sword"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 76618647335748,
+        ["Class"] = "Swords"
+    },
+    ["Emerald Sword"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 124866904658568,
+        ["Class"] = "Swords"
+    },
+    ["Pan"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 84594952015268,
+        ["Class"] = "Swords"
+    },
+    ["Wooden Mace"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 100737934118606,
+        ["Class"] = "Mace"
+    },
+    ["Stone Mace"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 113192818873849,
+        ["Class"] = "Mace"
+    },
+    ["Iron Mace"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 71854358328563,
+        ["Class"] = "Mace"
+    },
+    ["Diamond Mace"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 74410969297057,
+        ["Class"] = "Mace"
+    },
+    ["Emerald Mace"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 137004297539487,
+        ["Class"] = "Mace"
+    },
+    ["Wooden Katana"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 109304070552709,
+        ["Class"] = "Katanas"
+    },
+    ["Stone Katana"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 107558534596430,
+        ["Class"] = "Katanas"
+    },
+    ["Iron Katana"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 94332193955105,
+        ["Class"] = "Katanas"
+    },
+    ["Diamond Katana"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 86563704013018,
+        ["Class"] = "Katanas"
+    },
+    ["Emerald Katana"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 89032487913640,
+        ["Class"] = "Katanas"
+    },
+    ["Wooden Dagger"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 106423285288564,
+        ["Class"] = "Daggers"
+    },
+    ["Stone Dagger"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 111283903796923,
+        ["Class"] = "Daggers"
+    },
+    ["Iron Dagger"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 113446056109924,
+        ["Class"] = "Daggers"
+    },
+    ["Diamond Dagger"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 130780419123047,
+        ["Class"] = "Daggers"
+    },
+    ["Emerald Dagger"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 125463285880001,
+        ["Class"] = "Daggers"
+    },
+    ["Wooden Battle Axe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 124782909657171,
+        ["Class"] = "Battle Axes"
+    },
+    ["Stone Battle Axe"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 113549257208256,
+        ["Class"] = "Battle Axes"
+    },
+    ["Iron Battle Axe"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 119322109237008,
+        ["Class"] = "Battle Axes"
+    },
+    ["Diamond Battle Axe"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 133114155214382,
+        ["Class"] = "Battle Axes"
+    },
+    ["Emerald Battle Axe"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 91101420637229,
+        ["Class"] = "Battle Axes"
+    },
+    ["Wooden Spear"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 120283549615110,
+        ["Class"] = "Spears"
+    },
+    ["Stone Spear"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 122970515292818,
+        ["Class"] = "Spears"
+    },
+    ["Iron Spear"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 105787233616671,
+        ["Class"] = "Spears"
+    },
+    ["Diamond Spear"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 133023415704252,
+        ["Class"] = "Spears"
+    },
+    ["Emerald Spear"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 73448492274857,
+        ["Class"] = "Spears"
+    },
+    ["Wooden Pickaxe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 94164950478347,
+        ["Class"] = "Pickaxes"
+    },
+    ["Stone Pickaxe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 73083625338354,
+        ["Class"] = "Pickaxes"
+    },
+    ["Iron Pickaxe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 83654808669493,
+        ["Class"] = "Pickaxes"
+    },
+    ["Diamond Pickaxe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 106370331540345,
+        ["Class"] = "Pickaxes"
+    },
+    ["Wooden Axe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 139021727885406,
+        ["Class"] = "Axes"
+    },
+    ["Stone Axe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 112591799671482,
+        ["Class"] = "Axes"
+    },
+    ["Iron Axe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 115931830486454,
+        ["Class"] = "Axes"
+    },
+    ["Diamond Axe"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 89659255140299,
+        ["Class"] = "Axes"
+    },
+    ["Bow"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 74746320665814,
+        ["Class"] = "Ranged"
+    },
+    ["Crossbow"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 116383950785960,
+        ["Class"] = "Ranged"
+    },
+    ["Arrow"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 109203250184437,
+        ["Class"] = "Projectiles"
+    },
+    ["Teleorb"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 81028047146228,
+        ["Class"] = "Projectiles"
+    },
+    ["Fireball"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 131448014339749,
+        ["Class"] = "Projectiles"
+    },
+    ["Hotdog"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 88237544440251,
+        ["Class"] = "Projectiles"
+    },
+    ["Explosive Arrow"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 79285638442833,
+        ["Class"] = "Projectiles"
+    },
+    ["Wool"] = {
+        ["Name"] = "#team Wool",
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 135893128798792,
+        ["Class"] = "Blocks"
+    },
+    ["Fake Block"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 116628550857697,
+        ["Class"] = "Blocks"
+    },
+    ["Red Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 93529173705513,
+        ["Class"] = "Blocks"
+    },
+    ["Orange Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 130491296732253,
+        ["Class"] = "Blocks"
+    },
+    ["Yellow Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 102585617629582,
+        ["Class"] = "Blocks"
+    },
+    ["Green Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 93677152890758,
+        ["Class"] = "Blocks"
+    },
+    ["Blue Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 76325236977320,
+        ["Class"] = "Blocks"
+    },
+    ["Purple Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 94468657008782,
+        ["Class"] = "Blocks"
+    },
+    ["Pink Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 115366090384004,
+        ["Class"] = "Blocks"
+    },
+    ["Brown Wool"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 109756073839986,
+        ["Class"] = "Blocks"
+    },
+    ["Stone Brick"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 109864810766733,
+        ["Class"] = "Blocks"
+    },
+    ["Wood Plank"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 117669904635183,
+        ["Class"] = "Blocks"
+    },
+    ["Blastproof Ceramic"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 131315896912637,
+        ["Class"] = "Blocks"
+    },
+    ["Obsidian"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 106319103823956,
+        ["Class"] = "Blocks"
+    },
+    ["Bomb"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 126258789187072,
+        ["Class"] = "Blocks"
+    },
+    ["Slime"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 115414187671813,
+        ["Class"] = "Blocks"
+    },
+    ["TNT"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 74771859069797,
+        ["Class"] = "Blocks"
+    },
+    ["Smoke TNT"] = {
+        ["Permanant"] = false,
+        ["Image"] = "rbxassetid://" .. 70568180681852,
+        ["Class"] = "Blocks"
+    },
+    ["Mega TNT"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 73078145396409,
+        ["Class"] = "Blocks"
+    },
+    ["TNT Sheep"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 113554425389133,
+        ["Class"] = "Blocks"
+    },
+    ["Chez Burger"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 93792794686711,
+        ["Class"] = "Food"
+    },
+    ["Wooden Armor"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 130660143062206,
+        ["Class"] = "Armor"
+    },
+    ["Iron Armor"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 99060792921007,
+        ["Class"] = "Armor"
+    },
+    ["Diamond Armor"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 114259234470012,
+        ["Class"] = "Armor"
+    },
+    ["Emerald Armor"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 81138506933846,
+        ["Class"] = "Armor"
+    },
+    ["Wooden Helmet"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 83240453165009,
+        ["Class"] = "Helmet",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Wooden Armor"
+    },
+    ["Iron Helmet"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 103924157584098,
+        ["Class"] = "Helmet",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Iron Armor"
+    },
+    ["Diamond Helmet"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 135144491481414,
+        ["Class"] = "Helmet",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Diamond Armor"
+    },
+    ["Emerald Helmet"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 108190057052477,
+        ["Class"] = "Helmet",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Emerald Armor"
+    },
+    ["Wooden Chestplate"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 108904768335636,
+        ["Class"] = "Chestplate",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Wooden Armor"
+    },
+    ["Iron Chestplate"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 91904126740333,
+        ["Class"] = "Chestplate",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Iron Armor"
+    },
+    ["Diamond Chestplate"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 139129018730279,
+        ["Class"] = "Chestplate",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Diamond Armor"
+    },
+    ["Emerald Chestplate"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 140360324614669,
+        ["Class"] = "Chestplate",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Emerald Armor"
+    },
+    ["Wooden Pants"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 89336062504237,
+        ["Class"] = "Pants",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Wooden Armor"
+    },
+    ["Iron Pants"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 115916897120315,
+        ["Class"] = "Pants",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Iron Armor"
+    },
+    ["Diamond Pants"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 112507400041610,
+        ["Class"] = "Pants",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Diamond Armor"
+    },
+    ["Emerald Pants"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 113547441556874,
+        ["Class"] = "Pants",
+        ["Inventory"] = "Armor",
+        ["Parent"] = "Emerald Armor"
+    },
+    ["Iron"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 85025441279462,
+        ["Class"] = "Resources"
+    },
+    ["Diamond"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 86058419725020,
+        ["Class"] = "Resources"
+    },
+    ["Emerald"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 136901116721658,
+        ["Class"] = "Resources"
+    },
+    ["Balloon"] = {
+        ["Permanant"] = false,
+        ["CanStoreInChest"] = true,
+        ["CanDrop"] = true,
+        ["Image"] = "rbxassetid://" .. 123661666501185,
+        ["Class"] = "Balloons"
+    },
+    ["Skull Balloon"] = {
+        ["Permanant"] = false,
+        ["CanDrop"] = false,
+        ["Image"] = "rbxassetid://" .. 71843278937687,
+        ["Class"] = "Balloons"
+    },
+    ["Jetpack"] = {
+        ["Permanant"] = true,
+        ["Image"] = "rbxassetid://" .. 96183395776000,
+        ["Class"] = "Backpack"
+    }
+}
+return v1
