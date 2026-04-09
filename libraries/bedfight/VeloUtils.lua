d15f428cff78aad7e34e5444ebc992f7f590deb3
sstvskids <staavstudios@gmail.com>
2026-04-09 14:42:16 +1000

    Fixed

---
+++ libraries/bedfight/VeloUtils.lua
@@ -0,0 +1,7 @@
+local VeloUtils = {
+    FuckVelo = function()
+    end,
+    Method = 1 -- 1 or 2
+}
+
+return VeloUtils
--- libraries/bridgeduel/ServerData.lua
+++ libraries/bridgeduel/ServerData.lua
@@ -1,16 +1,14 @@
 local cloneref = cloneref or function(obj)
-	return obj
+    return obj
 end
 
 local Services = setmetatable({}, {
-	__index = function(self, obj)
-		return cloneref(game:GetService(obj))
-	end
+    __index = function(self, obj)
+        return cloneref(game:GetService(obj))
+    end
 })
 
 local ReplicatedStorage = Services.ReplicatedStorage
 local HttpService = Services.HttpService
 
-return {
-  Submode = HttpService:JSONDecode(ReplicatedStorage.Modules.ServerData.Cache.Value)
-}
+return HttpService:JSONDecode(ReplicatedStorage.Modules.ServerData.Cache.Value)
