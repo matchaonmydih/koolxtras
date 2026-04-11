b29799bd6ff0f1213f1d7fe2d1db69f7118fd0bb
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-11 11:02:18 +1000

    Create hoplex.lua

---
+++ games/hoplex.lua
@@ -0,0 +1,35 @@
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local Services = setmetatable({}, {
+	__index = function(self, obj)
+		return cloneref(game:GetService(obj))
+	end
+})
+
+local ReplicatedStorage = Services.ReplicatedStorage
+local UserInputService = Services.UserInputService
+local HttpService = Services.HttpService
+local Players = Services.Players
+local lplr = Players.LocalPlayer
+
+local function downloadFile(file)
+    url = file:gsub('koolaid/', '')
+    if not isfile(file) then
+        writefile(file, game:HttpGet('https://raw.githubusercontent.com/sstvskids/koolxtras/'..readfile('koolaid/commit.txt')..'/'..url))
+    end
+
+    repeat task.wait() until isfile(file)
+    return readfile(file)
+end
+
+local Library = shared.Library
+
+local Functions = loadstring(downloadFile('koolaid/libraries/functions.lua'))()
+local Raycast = loadstring(downloadFile('koolaid/libraries/raycast.lua'))()
+local Entity = loadstring(downloadFile('koolaid/libraries/entity.lua'))()
+
+local Dependencies = {
+    Client = Functions.require(ReplicatedStorage.Shared.ClientRuntime.Client)
+}
