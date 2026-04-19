7bb9ad36f63faacc759a6cce4efe0ba3f2a44537
sstvskids <staavstudios@gmail.com>
2026-04-19 13:58:02 +1000

    ok

---
+++ games/skyrivals.lua
@@ -0,0 +1,43 @@
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local Services = setmetatable({}, {
+	__index = function(self, obj)
+		return cloneref(game:GetService(obj))
+	end
+})
+
+local ReplicatedStorage = Services.ReplicatedStorage
+local UserInputService = Services.UserInputService
+local HttpService = Services.HttpService
+local Workspace = Services.Workspace
+local Players = Services.Players
+local lplr = Players.LocalPlayer
+
+local function downloadFile(file)
+    url = file:gsub('koolaid/', '')
+    if not isfile(file) then
+        writefile(file, game:HttpGet('https://raw.githubusercontent.com/sstvskids/koolxtras/'..readfile('koolaid/commit.txt')..'/'..url))
+    end
+
+    repeat task.wait() until isfile(file)
+    return readfile(file)
+end
+
+local Library = shared.Library
+
+local Execution = loadstring(downloadFile('koolaid/libraries/Execution.lua'))()
+local Functions = loadstring(downloadFile('koolaid/libraries/functions.lua'))()
+local Raycast = loadstring(downloadFile('koolaid/libraries/raycast.lua'))()
+local Entity = loadstring(downloadFile('koolaid/libraries/entity.lua'))()
+
+local Dependencies = {
+    Paths = {
+        Remotes = {
+            Attack = ReplicatedStorage.Remotes.ValidateHit,
+            DestroyBlock = ReplicatedStorage.Events.DestroyBlock
+        },
+        Knockback = ReplicatedStorage.Remotes.Knockback
+    }
+}
